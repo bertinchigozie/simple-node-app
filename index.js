5d9b22cd4108ba088b38057f87557a8464625e93
bertinchigozie <bertinchigozie@gmail.com>
2022-10-28 21:55:53 +0100

    fouth commit

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const fs = require("fs");
-const http = require("http");
+const express = require("express");
 
+const app = express();
 // Reading our json file
-const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const data = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
 
-// Creating the web server
-const server = http.createServer((req, res) => {
-  const endpoint = req.url;
-  if (endpoint === "/" || endpoint === "/api") {
-    res.writeHead(200, {
-      "Content-type": "application/json",
-    });
-
-    res.end(data);
-  } else {
-    res.end("<h1>Page Not Found</h1>");
-  }
+app.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+app.get("/api", (req, res) => {
+  res.status(200).json(data);
 });
 
 // Listening on a port
 const port = 8000;
-server.listen(port, "localhost");
+app.listen(port, "localhost");
