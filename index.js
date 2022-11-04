fb3eccb3efa245137eeaacb23b35b2efb6b09020
bertinchigozie <bertinchigozie@gmail.com>
2022-11-04 20:07:15 +0100

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const express = require("express");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 const app = express();
+
 // READING OUR JSON FILE
 const data = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
-
+app.use(express.json());
 // OUTPUTING THE JSON FILE
 app.get("/", (req, res) => {
   res.type("json");
@@ -14,6 +15,36 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.type("json");
   res.status(200).send(data);
+});
+
+app.post("/api", (req, res) => {
+  if (req.body.operation_type === "+") {
+    res.type("json");
+    res.status(201);
+    res.send({
+      slackname: "Godzie",
+      operation_type: req.body.operation_type,
+      result: Number(req.body.x) * Number(req.body.y),
+    });
+  }
+  if (req.body.operation_type === "-") {
+    res.type("json");
+    res.status(201);
+    res.send({
+      slackname: "Godzie",
+      operation_type: req.body.operation_type,
+      result: Number(req.body.x) * Number(req.body.y),
+    });
+  }
+  if (req.body.operation_type === "*") {
+    res.type("json");
+    res.status(201);
+    res.send({
+      slackname: "Godzie",
+      operation_type: req.body.operation_type,
+      result: Number(req.body.x) * Number(req.body.y),
+    });
+  }
 });
 
 // Listening on a port
