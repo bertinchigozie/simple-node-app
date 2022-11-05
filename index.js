2babf166680e2298b6584934fd44a78890af4761
bertinchigozie <bertinchigozie@gmail.com>
2022-11-05 06:22:36 +0100

    changed new commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/api", (req, res) => {
   res.status(200).send(data);
 });
 
-app.post("/api/post", (req, res) => {
+app.post("/api", (req, res) => {
   if (req.body.operation_type === "addition") {
     res.type("json");
     res.status(200);
